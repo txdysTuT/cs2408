98032dbb7f297257d878570e4beb3f88f42a4590
ztsubaki <2949125232@qq.com>
2024-12-02 21:44:57 +0800

    Remove recommendation.

--- js/index.js
+++ js/index.js
@@ -13,7 +13,21 @@ $.ajax({
                 let dormitory_member = dormitory.members[member];
                 if (!dormitory_member.visited) {
                     dormitory_member.dormintory = dormitory.name;
-                    re.randoms[k] = dormitory_member;
+                    re.randoms[0][k] = dormitory_member;
+                    dormitory.members[member].visited = true;
+                    break;
+                }
+            }
+        }
+        for (var k = 0; k < re.rand_num; k++) {
+            while (true) {
+                let dormitory = rnd(i++) % re.dormitories.length;
+                dormitory = re.dormitories[dormitory];
+                let member = rnd(i++) % dormitory.members.length;
+                let dormitory_member = dormitory.members[member];
+                if (!dormitory_member.visited) {
+                    dormitory_member.dormintory = dormitory.name;
+                    re.randoms[1][k] = dormitory_member;
                     dormitory.members[member].visited = true;
                     break;
                 }
@@ -35,14 +49,14 @@ window.onload = function () {
     }).mount("#vm");
     let py = $(document).scrollTop();
     $(window).scroll(function () {
-        if(($(document).scrollTop() > $("#title").height()))
-        {
-            if(($(document).scrollTop()-py)<-10){
-                $("#nav_bar").removeClass("nav_invisible").addClass("nav_visible");}
-            if(($(document).scrollTop()-py)>10){
+        if (($(document).scrollTop() > $("#title").height())) {
+            if (($(document).scrollTop() - py) < -5) {
+                $("#nav_bar").removeClass("nav_invisible").addClass("nav_visible");
+            }
+            if (($(document).scrollTop() - py) > 5) {
                 $("#nav_bar").removeClass("nav_visible").addClass("nav_invisible");
             }
-        }else{
+        } else {
             $("#nav_bar").removeClass("nav_visible").addClass("nav_invisible");
         }
         py = $(document).scrollTop();
@@ -52,9 +66,10 @@ window.onload = function () {
 
 function rnd(seed) {
     const d = new Date();
-    seed = d.getFullYear() * 8928 + d.getMonth() * 744 + d.getDate()*24 + d.getHours()*9 + seed*7;
+    seed = d.getFullYear() * 8928 + d.getMonth() * 744 + d.getDate() * 24 + d.getHours() * 9 + seed * 7;
     seed = (seed * 9301 + 49297) % 233280;
-    seed = Math.ceil(seed%10000 + seed/10000);
-    seed = (seed * 9301 + 49297) % 233280;2
+    seed = Math.ceil(seed % 10000 + seed / 10000);
+    seed = (seed * 9301 + 49297) % 233280;
+    2
     return seed;
 }
